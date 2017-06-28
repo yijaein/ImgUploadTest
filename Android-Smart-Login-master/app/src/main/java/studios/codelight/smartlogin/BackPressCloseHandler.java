c0653e9fc58925accec45d57e108f1c265e83222
DESKTOP-TDDC3KC\Jan <jilee317@naver.com>
2017-06-28 23:10:09 +0900

    06_28 development start viewpager, login

---
+++ Android-Smart-Login-master/app/src/main/java/studios/codelight/smartlogin/BackPressCloseHandler.java
@@ -0,0 +1,50 @@
+package studios.codelight.smartlogin;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.widget.Toast;
+
+/**
+ * Created by Jan on 2017-06-28.
+ */
+
+public class BackPressCloseHandler {
+    private long backKeyPressedTime = 0;
+    private Toast toast;
+
+    private Activity activity;
+
+    public BackPressCloseHandler(Activity context) {
+        this.activity = context;
+    }
+
+    public void onBackPressed() {
+        if (System.currentTimeMillis() > backKeyPressedTime + 2000) {
+            backKeyPressedTime = System.currentTimeMillis();
+            showGuide();
+            return;
+        }
+        if (System.currentTimeMillis() <= backKeyPressedTime + 2000) {
+            toast.cancel();
+
+            Intent t = new Intent(activity, MainActivity.class);
+            activity.startActivity(t);
+
+            activity.moveTaskToBack(true);
+            activity.finish();
+
+            /*
+            android.os.Process.killProcess(android.os.Process.myPid());
+            종료
+             */
+        }
+    }
+
+    public void showGuide() {
+        toast = Toast.makeText(activity, "한번 더 누르시면 종료됩니다.", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+
+
+}
---
+++ Android-Smart-Login-master/app/src/main/java/studios/codelight/smartlogin/Main2Activity.java
@@ -0,0 +1,152 @@
+package studios.codelight.smartlogin;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Main2Activity extends ActionBarActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    private BackPressCloseHandler backPressCloseHandler;
+    int MAX_PAGE = 3;                         //View Pager의 총 페이지 갯수를 나타내는 변수 선언
+    Fragment cur_fragment = new Fragment();   //현재 Viewpager가 가리키는 Fragment를 받을 변수 선언
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        backPressCloseHandler = new BackPressCloseHandler(this);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);        //Viewpager 선언 및 초기화
+        viewPager.setAdapter(new adapter(getSupportFragmentManager()));     //선언한 viewpager에 adapter를 연결
+
+
+    }//oncreate end
+
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            backPressCloseHandler.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main2, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+    private class adapter extends FragmentPagerAdapter {                    //adapter클래스
+        public adapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            if (position < 0 || MAX_PAGE <= position)        //가리키는 페이지가 0 이하거나 MAX_PAGE보다 많을 시 null로 리턴
+                return null;
+            switch (position) {              //포지션에 맞는 Fragment찾아서 cur_fragment변수에 대입
+                case 0:
+                    cur_fragment = new page_1();
+                    break;
+
+                case 1:
+                    cur_fragment = new page_2();
+                    break;
+
+                case 2:
+                    cur_fragment = new page_3();
+                    break;
+            }
+
+            return cur_fragment;
+        }
+
+        @Override
+        public int getCount() {
+            return MAX_PAGE;
+        }
+    }
+}
